40f24933660461dfb3661db8f997200c8b1a3361
cpancake <anrogers529@gmail.com>
2015-02-21 14:09:46 -0500

    Added Vector3 constants for directions.

--- Dunamis/Vector3.cs
+++ Dunamis/Vector3.cs
@@ -14,6 +14,14 @@ namespace Dunamis
         public static readonly Vector3 Zero = new Vector3(0, 0, 0);
         public static readonly Vector3 One = new Vector3(1, 1, 1);
 
+        // Directions
+        public static readonly Vector3 Up = new Vector3(0, 1, 0);
+        public static readonly Vector3 Down = new Vector3(0, -1, 0);
+        public static readonly Vector3 Front = new Vector3(0, 0, -1);
+        public static readonly Vector3 Back = new Vector3(0, 0, 1);
+        public static readonly Vector3 Left = new Vector3(-1, 0, 0);
+        public static readonly Vector3 Right = new Vector3(1, 0, 0);
+
         #region Properties
         public float Length
         {
